37cd6523e93af025d8eb5d4d252d0e28107f95d1
Владислав Бодин <vladislavbodin@MBP-Vladislav.lan>
2020-02-10 08:10:34 +0300

    Подготовил вёрстку для карусели. Тестировано только на десктопе

---
+++ source/js/test.js
@@ -0,0 +1,17 @@
+// 'use strict';
+
+// function makeMoreTxt(nodes) {
+//   for(let i = 0; i < nodes.length; i++) {
+//     console.dir(nodes[i],1);
+//     nodes[i].textContent = nodes[i].textContent.repeat(3);
+//   }
+// };
+
+// let paragraphs = document.querySelectorAll('p');
+// makeMoreTxt(paragraphs);
+
+// let links = document.querySelectorAll('a');
+// makeMoreTxt(links);
+
+// let spans = document.querySelectorAll('span');
+// makeMoreTxt(spans);
